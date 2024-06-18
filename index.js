0c033820c5b52675a162b1c869408ba98eb37e6a
Richard Hocking <141461457+rhocking122@users.noreply.github.com>
2024-06-18 15:56:00 -0500

    step 3

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
+//way to find an average of an array
+/* function getAverage(scores) {
+   let sum = 0;
+   for (const score of scores) {
+     sum += score;
+   }
+   return sum / scores.length;
+ } */
+
+// another way to find the average of an array
+
 function getAverage(scores) {
   let sum = 0;
-
-  for (const score of scores) {
-    sum += score;
+  for (let i=0; i < scores.length; i++) {
+    sum += scores[i];
   }
-
   return sum / scores.length;
-}
+};
 
-function getGrade(score) {
-  if (score === 100) {
-    return "A++";
-  } else if (score >= 90) {
-    return "A";
-  } else if (score >= 80) {
-    return "B";
-  } else if (score >= 70) {
-    return "C";
-  } else if (score >= 60) {
-    return "D";
+function getGrade(scores) {
+  if (scores === 100){
+    return "A++"
+  } else if (scores >= 90) {
+    return "A"
+  } else if (scores >= 80) {
+    return "B"
+  } else if (scores >= 70) {
+    return "C"
+  } else if (scores >=60) {
+    return "D"
   } else {
-    return "F";
+    return "F"
   }
+};
+
+function hasPassingGrade(scores) {
+  return getGrade(scores) !== "F";
 }
 
-function hasPassingGrade(score) {
-  return getGrade(score) !== "F";
-}
-
-
-// User Editable Region
-
-function studentMsg(totalScores, studentScore) {
-if (hasPassingGrade(getGrade(studentScore))) {
-      return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You passed the course.";
-    } else {
-      return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You failed the course.";
-    }
-  }
-
-console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(studentMsg([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+console.log(getGrade(100));
+console.log(getGrade(92));
+console.log(getGrade(63));
+console.log(getGrade(83));
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));

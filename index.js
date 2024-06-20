5a40bcc54f17d80280fccf524a063db0bf157a43
Richard Hocking <141461457+rhocking122@users.noreply.github.com>
2024-06-20 16:00:04 -0500

    finished

--- index.js
+++ index.js
@@ -37,12 +37,17 @@ function hasPassingGrade(scores) {
   return getGrade(scores) !== "F";
 }
 
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
-console.log(getGrade(100));
-console.log(getGrade(92));
-console.log(getGrade(63));
-console.log(getGrade(83));
-console.log(hasPassingGrade(100));
-console.log(hasPassingGrade(53));
-console.log(hasPassingGrade(87));
+function studentMsg(totalScores, studentScore) {
+  const classAverage = getAverage(totalScores);
+  const studentGrade = getGrade(studentScore);
+  const passed = hasPassingGrade(studentScore);
+
+  if (passed) {
+    return `Class average: ${classAverage}. Your grade: ${studentGrade}. You passed the course.`;
+  } else {
+    return `Class average: ${classAverage}. Your grade: ${studentGrade}. You failed the course.`;
+  }
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 47)); // "Class average: 71.7. Your grade: D. You failed the course."
+console.log(studentMsg([45, 87, 98, 100, 86, 94, 67, 88, 94, 95], 95));
